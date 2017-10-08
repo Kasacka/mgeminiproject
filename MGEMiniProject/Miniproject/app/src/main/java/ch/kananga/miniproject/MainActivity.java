b6de769852c1952e8edcae6fd8e3117d4024583c
Joel Egger <joelegger@Joels-MacBook-Pro.local>
2017-10-08 21:09:45 +0200

    bugfix MainActivity

--- MGEMiniProject/Miniproject/app/src/main/java/ch/kananga/miniproject/MainActivity.java
+++ MGEMiniProject/Miniproject/app/src/main/java/ch/kananga/miniproject/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button reservationViewButton;
+    private Button registerButton;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +16,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         
         reservationViewButton = (Button) findViewById(R.id.reservation_view_button);
+        registerButton = (Button) findViewById(R.id.register_button);
+
         reservationViewButton.setOnClickListener(this);
+        registerButton.setOnClickListener(this);
     }
     
     @Override
     public void onClick(View view) {
         if (view == reservationViewButton) {
             onReservationViewButtonClick();
+        } else if (view == registerButton) {
+            onRegisterButtonClick();
+        } else {
+            throw new AssertionError("event handler not implemented");
         }
     }
 
     private void onReservationViewButtonClick() {
         startActivity(new Intent(this, ReservationActivity.class));
+    }
+
+    private void onRegisterButtonClick() {
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
