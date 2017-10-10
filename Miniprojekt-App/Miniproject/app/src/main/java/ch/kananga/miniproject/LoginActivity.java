21a41cd0789025b2b016edeb4c095eead3691483
David Loosli <dloosli@hsr.ch>
2017-10-10 10:40:01 +0200

    added login screen design without functionality

--- Miniprojekt-App/Miniproject/app/src/main/java/ch/kananga/miniproject/LoginActivity.java
+++ Miniprojekt-App/Miniproject/app/src/main/java/ch/kananga/miniproject/LoginActivity.java
@@ -6,41 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import ch.kananga.miniproject.ui.RegisterActivity;
 import ch.kananga.miniproject.ui.ReservationActivity;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button reservationViewButton;
-    private Button registerButton;
+    private Button loginButton;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        
-        reservationViewButton = (Button) findViewById(R.id.reservation_view_button);
-        registerButton = (Button) findViewById(R.id.register_button);
 
-        reservationViewButton.setOnClickListener(this);
-        registerButton.setOnClickListener(this);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
     }
     
     @Override
     public void onClick(View view) {
-        if (view == reservationViewButton) {
-            onReservationViewButtonClick();
-        } else if (view == registerButton) {
+        if (view == loginButton) {
             onRegisterButtonClick();
         } else {
             throw new AssertionError("event handler not implemented");
         }
     }
 
-    private void onReservationViewButtonClick() {
-        startActivity(new Intent(this, ReservationActivity.class));
-    }
-
     private void onRegisterButtonClick() {
-        startActivity(new Intent(this, RegisterActivity.class));
+        startActivity(new Intent(this, ReservationActivity.class));
     }
 }
