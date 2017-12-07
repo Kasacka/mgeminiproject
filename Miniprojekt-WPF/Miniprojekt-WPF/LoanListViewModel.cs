d65865e67b3c57830cb6a39b904886e17ea76c1c
LAPTOP-2A3MPK48\michelle <michelle.kunz@hsr.ch>
2017-12-07 14:41:12 +0100

    ADD loan view and list styles

---
+++ Miniprojekt-WPF/Miniprojekt-WPF/LoanListViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace Miniprojekt_WPF
+{
+    public class LoanListViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Loan> loanList;
+        private LibraryAdminService libraryAdminService;
+
+        public LoanListViewModel()
+        {
+            loanList = new ObservableCollection<Loan>();
+            var serverAddress = ConfigurationManager.AppSettings["server"];
+            libraryAdminService = new LibraryAdminService(serverAddress);
+            libraryAdminService.GetAllLoans().ForEach(loanList.Add);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Loan> LoanList
+        {
+            get { return loanList; }
+            set
+            {
+                loanList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string properyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(properyName));
+        }
+    }
+}
