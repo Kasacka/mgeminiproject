8e85a32d5dca597824ce9ae1b91b290f0e6e6fef
LAPTOP-2A3MPK48\michelle <michelle.kunz@hsr.ch>
2017-12-14 13:09:45 +0100

    add LoanViewModel class

--- Miniprojekt-WPF/Miniprojekt-WPF/ViewModel/LoanListViewModel.cs
+++ Miniprojekt-WPF/Miniprojekt-WPF/ViewModel/LoanListViewModel.cs
@@ -1,28 +1,30 @@
 ﻿using ch.hsr.wpf.gadgeothek.domain;
 using ch.hsr.wpf.gadgeothek.service;
+using Miniprojekt_WPF.ViewModel;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Configuration;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Miniprojekt_WPF
 {
     public class LoanListViewModel : INotifyPropertyChanged
     {
-        private ObservableCollection<Loan> loanList;
+        private ObservableCollection<LoanViewModel> loanList;
         private LibraryAdminService libraryAdminService;
 
         public LoanListViewModel()
         {
-            loanList = new ObservableCollection<Loan>();
+            loanList = new ObservableCollection<LoanViewModel>();
             var serverAddress = ConfigurationManager.AppSettings["server"];
             libraryAdminService = new LibraryAdminService(serverAddress);
-            libraryAdminService.GetAllLoans().ForEach(loanList.Add);
+            libraryAdminService.GetAllLoans().Select(loan => new LoanViewModel(loan)).ToList().ForEach(loanList.Add);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ObservableCollection<Loan> LoanList
+        public ObservableCollection<LoanViewModel> LoanList
         {
             get { return loanList; }
             set
